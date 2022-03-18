b47feff6701fe5f35c7e1d88805f4c053e260c2b
iq-mschmeets <mark@inquisient.com>
2022-03-18 17:17:08 -0400

    comments

--- index.js
+++ index.js
@@ -3,26 +3,6 @@ import './style.css';
 import { listenOnce, observeMouseOutsideOfContainer } from './utils.js';
 import { makeEditor } from './editor.js';
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById('myDropdown').classList.toggle('show');
-}
-
-// Close the dropdown menu if the user clicks outside of it
-// window.onclick = function (event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName('dropdown-content');
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// };
-
 const cc = document.querySelector('#center-column');
 cc.appendChild(makeEditor());
 
@@ -49,9 +29,15 @@ const sourceOnFocus = (evt) => {
   }
   isEditing = true;
 };
+
+// The p.rel in the document, these are the "target" elements
+// to host the editor.
 const sources = Array.from(
   document.querySelector('.stamp').querySelectorAll('p.rel')
 );
+
+// Add a listener to each of the p.rel's in the stamp.
+// When they get focus, we'll overlay the editor.
 sources.forEach((p) => p.addEventListener('focus', sourceOnFocus));
 
 console.log('========== end of file ==========');
